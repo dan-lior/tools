bd2e2112dfcfeedff02b055d1c454b5e651f2cb5
dan lior <dan.lior@bcm.edu>
2022-07-06 14:43:01 -0500

    minor changes

--- MRI/nondicom_reader/main.cpp
+++ MRI/nondicom_reader/main.cpp
@@ -7,21 +7,20 @@
 
 int main()
 {
-    std::cerr << "hello" << std::endl;
+    // converts nondicom timeslices to vtk timeslices
 
-    const std::string nondicom_directory("/home/dan/git-repos/tools/MRI/nondicom_data/patient2_nondicom_data/");
-    std::vector<LabelledGrid<3, 3, Vector<3>>> gwa_timeslices = read_nondicom(nondicom_directory);
-
+    const std::string nondicom_directory("/home/dan/git-repos/tools/MRI/nondicom_data/patient2/");
     const std::string vtk_directory("/home/dan/git-repos/tools/MRI/vtk_data/patient2/");
 
-    for (uint64_t i=0; i<gwa_timeslices.size(); ++i)
+    std::vector<LabelledGrid<3, 3, Vector<3>>> velocity_timeslices = read_nondicom(nondicom_directory);
+
+    for (uint64_t i=0; i<velocity_timeslices.size(); ++i)
     {
         std::string filename(vtk_directory + std::to_string(i) + std::string(".vtk"));
         std::cerr << "about to export timeslice " << i << std::endl;
-        gwa_timeslices[i].export_to_vtk(filename);
+        velocity_timeslices[i].export_to_vtk(filename);
         std::cerr << "finished exporting timeslice " << i  << std::endl;
     }
 
-    std::cerr << "goodbye" << std::endl;
     return 0;
 }
